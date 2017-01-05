9756d7a116244bac0888d1b10bc06e3fbc3a5bc8
吴喆 <wuzhe@beisendeMacBook-Air-3.local>
2017-01-05 10:58:11 +0800

    test

--- 1.py
+++ 1.py
@@ -1,1 +1,14 @@
-#
+#oh they say people come, they say people go. 
+#人言聚有时，散有期。
+#This particular diamond was extra special 
+#而回忆却会永恒闪耀在漫漫星辰里
+#And though you might be gone 
+#我知道你已离我而去
+#And the world may not know 
+#尽管这世界依旧正常转动正常运行
+#Still I see you, celestial 
+#而我仿佛还能每天看到你 如银汉星河那般的美丽
+#Like a lion you ran 
+#动如温婉脱兔 奔如雌狮引我侧目
+#A Goddess you rolled 
+#任何人笔下的女神都难将你临摹
